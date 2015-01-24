da31985f65b5350dc186dc25dcb75f31bc47ca7b
Raphael Randschau <nicolai86@me.com>
2015-01-24 17:55:45 +0100

    Update UI to list recipes, simple recipe form

--- app/scripts/app.js
+++ app/scripts/app.js
@@ -15,7 +15,9 @@ angular
   'ngRoute',
   'ngSanitize'
 ])
-.config(function($routeProvider) {
+.config(function($routeProvider, $resourceProvider) {
+  $resourceProvider.defaults.stripTrailingSlashes = false;
+
   $routeProvider
   .when('/', {
     templateUrl: 'views/calendar.html',
@@ -24,6 +26,14 @@ angular
   .when('/recipes', {
     templateUrl: 'views/recipes.html',
     controller: 'RecipesCtrl'
+  })
+  .when('/recipes/new', {
+    templateUrl: 'views/recipes.edit.html',
+    controller: 'NewRecipeCtrl'
+  })
+  .when('/recipes/:id/edit', {
+    templateUrl: 'views/recipes.edit.html',
+    controller: 'EditRecipeCtrl'
   })
   .when('/about', {
     templateUrl: 'views/about.html',
--- app/scripts/controllers/recipes.js
+++ app/scripts/controllers/recipes.js
@@ -4,14 +4,37 @@
  * @ngdoc function
  * @name foodPlanOrganizerApp.controller:MainCtrl
  * @description
- * # MainCtrl
+ * # RecipesCtrl
  * Controller of the foodPlanOrganizerApp
  */
 angular.module('foodPlanOrganizerApp')
-.controller('RecipesCtrl', function($scope) {
-  $scope.awesomeThings = [
-    'HTML5 Boilerplate',
-    'AngularJS',
-    'Karma'
-  ];
+.factory('Recipe', ['$resource', function($resource) {
+    return $resource('http://localhost:8080/recipes/', {
+      id: '@id'
+    });
+  }])
+.controller('RecipesCtrl', function($scope, Recipe) {
+  $scope.recipes = Recipe.get();
+
+  $scope.deleteRecipe = function(recipeId) {
+    // TODO
+  };
+})
+.controller('EditRecipeCtrl', function($scope, $routeParams, Recipe) {
+  console.log('EditRecipe');
+  $scope.recipe = Recipe.get({
+    id: $routeParams.id
+  });
+  $scope.persist = function() {
+    console.log('TODO UPDATE');
+  };
+  // TODO
+})
+.controller('NewRecipeCtrl', function($scope, Recipe) {
+  console.log('NewRecipe');
+  $scope.recipe = {};
+  $scope.persist = function() {
+    console.log('TODO CREATE');
+  };
+  // TODO
 });
