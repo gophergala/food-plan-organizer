1e2589f34b8835698662f68b347361617ad30ef1
Raphael Randschau <nicolai86@me.com>
2015-01-25 14:12:21 +0100

    Add fake calendar view
    this probably won't be finished in time

--- app/scripts/controllers/calendar.js
+++ app/scripts/controllers/calendar.js
@@ -7,11 +7,28 @@
  * # MainCtrl
  * Controller of the foodPlanOrganizerApp
  */
+
+function getMonday(d) {
+  d = new Date(d);
+  var day = d.getDay(),
+    diff = d.getDate() - day + (day === 0 ? -6 : 1);
+  return new Date(d.setDate(diff));
+}
+var getWeek = function(weekDiff) {
+  var startDate = new Date();
+  startDate = startDate.setDate(startDate.getDate() + weekDiff * 7);
+  startDate = getMonday(startDate);
+
+  var dates = [];
+  for (var d = 0; d < 7; d++) {
+    var date = new Date(startDate.getTime());
+    date.setDate(date.getDate() + d);
+    dates.push(date);
+  }
+  return dates;
+}
+
 angular.module('foodPlanOrganizerApp')
 .controller('CalendarCtrl', function($scope) {
-  $scope.awesomeThings = [
-    'HTML5 Boilerplate',
-    'AngularJS',
-    'Karma'
-  ];
+  $scope.weeks = [getWeek(0), getWeek(1), getWeek(2)];
 });
